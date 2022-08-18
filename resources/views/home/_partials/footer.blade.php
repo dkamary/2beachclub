e811109ff3f6ad95122b6532297842049a4dccfb
Donatien KAMARY <donatkamary@gmail.com>
2022-08-18 11:42:36 +0400

    new line

--- resources/views/home/_partials/footer.blade.php
+++ resources/views/home/_partials/footer.blade.php
@@ -5,7 +5,7 @@
         <p class="text-center">
             More information on <br>
             <a href="mailto:sales@2futures.com">sales@2futures.com</a>
-            {{-- or <a href="tel:+23059774880">(+230) 5977 4880</a> <br> --}}
+            {{-- or <a href="tel:+23059774880">(+230) 5977 4880</a> --}}<br>
             <a href="http://2futures.com">2futures.com</a>
         </p>
     </section>
