2d9a9daa5899c8fecc7a4f0e9d20f0aa7693c757
Donatien KAMARY <donatkamary@gmail.com>
2023-07-25 15:47:59 +0400

    Update

--- resources/views/home/sections/membership-table.blade.php
+++ resources/views/home/sections/membership-table.blade.php
@@ -6,7 +6,7 @@
             <th style="padding-top: 12px; padding-bottom: 12px;">
                 &nbsp;
             </th>
-            <th colspan="4" style="padding-top: 12px; padding-bottom: 12px;">
+            <th colspan="3" style="padding-top: 12px; padding-bottom: 12px;">
                 <h3>Membership tier</h3>
             </th>
         </tr>
@@ -19,12 +19,12 @@
             <th width="20%" class="membership silver">Silver</th>
             {{-- <th width="20%" class="membership day-full">Day<br>(full access)</th>
             <th width="20%" class="membership day-resto">Day<br>(restaurant)</th> --}}
-            <th width="20%" class="membership tenants">Tenants<br>(2F Holidays)</th>
+            {{-- <th width="20%" class="membership tenants">Tenants<br>(2Futures Holidays)</th> --}}
         </tr>
     </thead>
     <tbody>
         <tr>
-            <td colspan="7">&nbsp;</td>
+            <td colspan="4">&nbsp;</td>
         </tr>
         <tr>
             <td class="thead">Annual Fee (USD)</td>
@@ -33,7 +33,7 @@
             <td class="membership silver">300</td>
             {{-- <td class="membership day-full"></td>
             <td class="membership day-resto"></td> --}}
-            <td class="membership tenants"></td>
+            {{-- <td class="membership tenants"></td> --}}
         </tr>
         {{-- <tr>
             <td class="thead">Annual Fee for Ki Residences owners (80% discount)</td>
@@ -60,16 +60,16 @@
             <td class="membership silver">4</td>
             {{-- <td class="membership day-full">1</td>
             <td class="membership day-resto">1</td> --}}
-            <td class="membership tenants"></td>
+            {{-- <td class="membership tenants"></td> --}}
         </tr>
         <tr>
-            <td colspan="6">&nbsp;</td>
+            <td colspan="4">&nbsp;</td>
         </tr>
         <tr>
             <td class="thead" style="padding-top: 12px">
                 <h3>Benefits</h3>
             </td>
-            <td colspan="4">&nbsp;</td>
+            <td colspan="3" style="background-color: #ffffff;">&nbsp;</td>
             {{-- <td class="membership platinium"></td>
             <td class="membership gold"></td>
             <td class="membership silver"></td> --}}
@@ -83,26 +83,26 @@
             <td class="membership gold">✔</td>
             <td class="membership silver">✔</td>
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto">✔</td> --}}
-            <td class="membership tenants">✔</td>
+            <td class="membership day-resto">✔</td>
+            <td class="membership tenants">✔</td> --}}
         </tr>
         <tr>
-            <td class="thead">Lounge (opening 2024)</td>
+            <td class="thead">Lounge<br>(opening 2024)</td>
             <td class="membership platinium">✔</td>
             <td class="membership gold">✔</td>
             <td class="membership silver">✔</td>
             {{-- <td class="membership day-full"><strong>-</strong></td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants"><strong>-</strong></td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants"><strong>-</strong></td> --}}
         </tr>
         <tr>
-            <td class="thead">Access to boardroom (opening 2024)</td>
+            <td class="thead">Access to boardroom<br>(opening 2024)</td>
             <td class="membership platinium">4 hours monthly</td>
             <td class="membership gold">2 hours monthly</td>
             <td class="membership silver">Extra charge</td>
             {{-- <td class="membership day-full"><strong>-</strong></td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants"><strong>-</strong></td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants"><strong>-</strong></td> --}}
         </tr>
         <tr>
             <td class="thead">Roof top bar</td>
@@ -110,8 +110,8 @@
             <td class="membership gold">✔</td>
             <td class="membership silver">✔</td>
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto">✔</td> --}}
-            <td class="membership tenants">✔</td>
+            <td class="membership day-resto">✔</td>
+            <td class="membership tenants">✔</td> --}}
         </tr>
         <tr>
             <td class="thead">Pool</td>
@@ -119,8 +119,8 @@
             <td class="membership gold">✔</td>
             <td class="membership silver">✔</td>
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants">✔</td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants">✔</td> --}}
         </tr>
         <tr>
             <td class="thead">Towel service</td>
@@ -128,8 +128,8 @@
             <td class="membership gold">✔</td>
             <td class="membership silver" rowspan="3">Subject to minimum consumption of <br>USD 25 per person</td>
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants" rowspan="3">Subject to minimum consumption of <br>USD 35 per person</td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants" rowspan="3">Subject to minimum consumption of <br>USD 35 per person</td>--}}
         </tr>
         <tr>
             <td class="thead">Locker room</td>
@@ -137,7 +137,7 @@
             <td class="membership gold">✔</td>
             {{-- <td></td> --}}
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
+            <td class="membership day-resto"><strong>-</strong></td>
             {{-- <td></td> --}}
         </tr>
         <tr>
@@ -146,7 +146,7 @@
             <td class="membership gold">✔</td>
             {{-- <td></td> --}}
             {{-- <td class="membership day-full">✔</td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
+            <td class="membership day-resto"><strong>-</strong></td>
             {{-- <td></td> --}}
         </tr>
         <tr>
@@ -155,17 +155,17 @@
             <td class="membership gold">✔</td>
             <td class="membership silver">Extra charge</td>
             {{-- <td class="membership day-full">Extra charge</td>
-            <td class="membership day-resto">Extra charge</td> --}}
-            <td class="membership tenants">Extra charge</td>
+            <td class="membership day-resto">Extra charge</td>
+            <td class="membership tenants">Extra charge</td> --}}
         </tr>
         <tr>
-            <td class="thead">Shuttle service to and from 2Beach Club - 2F residences (Bain Boeuf to Trou aux Biches)</td>
+            <td class="thead">Shuttle service to and from 2Beach Club - 2Futures residences (Bain Boeuf to Trou aux Biches)</td>
             <td class="membership platinium">✔</td>
             <td class="membership gold"><strong>-</strong></td>
             <td class="membership silver"><strong>-</strong></td>
             {{-- <td class="membership day-full"><strong>-</strong></td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants">✔</td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants">✔</td> --}}
         </tr>
         <tr>
             <td class="thead">Guest passes</td>
@@ -173,17 +173,17 @@
             <td class="membership gold">5</td>
             <td class="membership silver">2</td>
             {{-- <td class="membership day-full"><strong>-</strong></td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants"><strong>-</strong></td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants"><strong>-</strong></td> --}}
         </tr>
         <tr>
-            <td class="thead">Access to tenants (if rented through 2F Holidays)</td>
+            <td class="thead">Access to tenants (if rented through 2Futures Holidays)</td>
             <td class="membership platinium">✔</td>
             <td class="membership gold"><strong>-</strong></td>
             <td class="membership silver"><strong>-</strong></td>
             {{-- <td class="membership day-full"><strong>-</strong></td>
-            <td class="membership day-resto"><strong>-</strong></td> --}}
-            <td class="membership tenants">N/A</td>
+            <td class="membership day-resto"><strong>-</strong></td>
+            <td class="membership tenants">N/A</td> --}}
         </tr>
         <tr>
             <td class="thead">Discount on F&B</td>
@@ -191,8 +191,8 @@
             <td class="membership gold">10%</td>
             <td class="membership silver">5%</td>
             {{-- <td class="membership day-full">0%</td>
-            <td class="membership day-resto">0%</td> --}}
-            <td class="membership tenants">0%</td>
+            <td class="membership day-resto">0%</td>
+            <td class="membership tenants">0%</td> --}}
         </tr>
     </tbody>
 </table>
@@ -228,6 +228,9 @@
 
             .membership-table .thead {
                 text-align: left;
+                background-color: #ffffff;
+                color: #333333;
+                padding-left: 8px;
             }
 
             .membership {
