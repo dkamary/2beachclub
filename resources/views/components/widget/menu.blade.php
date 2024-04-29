5d6ec75c2ebf7216c3b0eb07dedda7e3d25489ab
Donatien Kamary <donatkamary@gmail.com>
2024-04-29 11:50:02 +0400

    Update https link

--- resources/views/components/widget/menu.blade.php
+++ resources/views/components/widget/menu.blade.php
@@ -19,7 +19,7 @@
     @endpush
 
     @push('foot')
-        <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
         <script src="{{ asset('plugins/fatNav/jquery.fatNav.js') }}"></script>
         <script>
             (function() {
