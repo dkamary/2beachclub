dbd28dcb39488a77c596954f637ac9f177d25240
Donatien Kamary <donatkamary@gmail.com>
2024-05-02 16:01:21 +0400

    sapcing

--- resources/views/v2/home.blade.php
+++ resources/views/v2/home.blade.php
@@ -8,11 +8,16 @@
         <x-v2.intro id="home" :class="['container', 'bg-white', 'py-5']" />
         <x-v2.opening-hours id="opening-hours" :class="['container', 'bg-white', 'py-5']" />
         <x-v2.restaurant-presentation id="restaurant" :class="['container', 'bg-white', 'py-5']" />
+        <div class="container bg-white py-4"></div>
         <x-v2.beach-pool id="beach-and-pool" :class="['container', 'bg-white', 'py-5']" />
+        <div class="container bg-white py-4"></div>
         <x-v2.events id="events" :class="['container', 'bg-white', 'py-5']" />
         <x-v2.upcoming-events id="upcoming-events" :class="['container', 'bg-white', 'py-5']" />
+        <div class="container bg-white py-4"></div>
         <x-v2.gallery id="gallery" :class="['container', 'bg-white', 'py-5']" />
+        <div class="container bg-white py-4"></div>
         <x-v2.contact-us id="contact-us" :class="['container', 'bg-white', 'py-5']" />
+        <div class="container bg-white py-4"></div>
         <x-v2.google-map id="find-us" :class="['container', 'bg-white', 'py-5']" />
 
     </x-layout.v2>
