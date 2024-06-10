c0efc695689d04bc1402fede7a3b82cad604babe
Donatien Kamary <donatkamary@gmail.com>
2024-06-10 11:59:40 +0400

    JS defer not async

--- resources/views/_partials/javascripts.blade.php
+++ resources/views/_partials/javascripts.blade.php
@@ -1,12 +1,12 @@
 {{-- Javascripts --}}
 
-<script src="{{ asset('v2/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('v2/js/bootstrap.bundle.min.js') }}" defer></script>
 
-<script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('js/lazyload.js')}}" defer async></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ asset('js/lazyload.js')}}" defer></script>
 @handheld
-    <script src="{{ asset('v2/jquery/jquery-3.7.1.min.js') }}"></script>
-    <script src="{{ asset('v2/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('v2/jquery/jquery-3.7.1.min.js') }}" defer></script>
+    <script src="{{ asset('v2/slick/slick.min.js') }}" defer></script>
     <script>
         (function($){
 
