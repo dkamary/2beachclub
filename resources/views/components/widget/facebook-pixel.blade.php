60ad603ba58435eb5e224d462b4e2e8c2ecf06f3
Donatien Kamary <donatkamary@gmail.com>
2024-07-05 16:37:19 +0400

    Updates: unique GTags

--- resources/views/components/widget/facebook-pixel.blade.php
+++ resources/views/components/widget/facebook-pixel.blade.php
@@ -4,22 +4,24 @@
     $key = $key ?? '{{ $key }}';
 @endphp
 
-<!-- Meta Pixel Code -->
-<script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window,document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '{{ $key }}');
-    fbq('track', 'PageView');
-</script>
-<noscript>
-    <img height="1" width="1"
-    src="https://www.facebook.com/tr?id={{ $key }}&ev=PageView
-    &noscript=1"/>
-</noscript>
-<!-- End Meta Pixel Code -->
+@once
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window,document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '{{ $key }}');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1"
+        src="https://www.facebook.com/tr?id={{ $key }}&ev=PageView
+        &noscript=1"/>
+    </noscript>
+    <!-- End Meta Pixel Code -->
+@endonce
--- resources/views/components/widget/google-analytics.blade.php
+++ resources/views/components/widget/google-analytics.blade.php
@@ -4,9 +4,13 @@
     $key = $key ?? 'G-KE6G8QJN02';
 @endphp
 
+@once
+
 <script async src="https://www.googletagmanager.com/gtag/js?id={{ $key }}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());gtag('config', '{{ $key }}');
 </script>
+
+@endonce
