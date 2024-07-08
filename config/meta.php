06e882a2b07175e2172a28a252da3eef28d4bd79
Donatien Kamary <donatkamary@gmail.com>
2024-07-08 11:48:22 +0400

    update: meta image preview

--- config/meta.php
+++ config/meta.php
@@ -7,13 +7,13 @@ return [
             'title' => '2Beach Club by 2Futures Mauritius',
             'url' => 'https://2beachclub.mu/',
             'description' => 'Live the 5 -star lifestyle everyday',
-            'preview' => 'img/section-1-1-2160w.webp' /*asset('img/section-1-1-2160w.webp')*/,
+            'preview' => 'v2/img/home-page/2beach-club-pool-view.webp' /*asset('img/section-1-1-2160w.webp')*/,
         ],
         'membership' => [
             'title' => 'Become a member - 2Beach Club by 2Futures Mauritius',
             'url' => 'https://2beachclub.mu/become-member',
             'description' => 'Become a member and live the 5 -star lifestyle everyday',
-            'preview' => 'img/section-1-1-2160w.webp' /*asset('img/section-1-1-2160w.webp')*/,
+            'preview' => 'v2/img/events/headers/2beach-club-saturday-sounds-sea-sunset-view-boats.webp' /*asset('img/section-1-1-2160w.webp')*/,
         ],
         'events' => [
             'title' => 'Events - 2Beach Club by 2Futures Mauritius',
