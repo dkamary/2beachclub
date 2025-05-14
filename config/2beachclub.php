561a9787f95fb3f65e586414cbcbd29e315aed3e
Donatien Kamary <donatkamary@gmail.com>
2025-05-14 09:40:12 +0400

    New menu

--- config/2beachclub.php
+++ config/2beachclub.php
@@ -13,10 +13,10 @@ return [
     'whatsapp-channel' => 'https://whatsapp.com/channel/0029VaDhFGQ4dTnLrv1vl83l',
     'menu'=> [
         'kids-menu' => 'downloads/Kids-Menu-2024-10-14.pdf',
-        'all-day-en' => 'downloads/All-Day-Dining-EN-2024-10-14.pdf',
-        'all-day-fr' => 'downloads/All-Day-Dining-FR-2024-10-14.pdf',
+        'all-day-en' => 'downloads/All-day-dining-Eng-2025-05.pdf',
+        'all-day-fr' => 'downloads/All-day-dining-FR-2025-05.pdf',
         'sushi' => 'downloads/Sushi-Menu-2024-10-14.pdf',
-        'sundowner' => 'downloads/Sundowner-Menu-2024-10-14.pdf',
+        'sundowner' => 'downloads/Sundowner-Menu-2025-05.pdf',
     ],
     'recaptcha' => [
         'key' => '6LccbLkqAAAAAOT34v6h0TRLxy_qsCliDkCts0-l',
