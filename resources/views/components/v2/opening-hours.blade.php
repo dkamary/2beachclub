f6e47838100c2c24cb354d2f67a5a4e9e4f6f008
Donatien Kamary <donatkamary@gmail.com>
2025-06-13 19:28:51 +0400

    Update opening hours

--- resources/views/components/v2/opening-hours.blade.php
+++ resources/views/components/v2/opening-hours.blade.php
@@ -28,7 +28,7 @@
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">7:30</span><span class="fs-5">PM</span>
+                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">9:30</span><span class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
                     <span class="fs-3 fw-bold text-uppercase">Mon</span>
@@ -55,7 +55,7 @@
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">11</span><span class="fs-5">AM</span> - <span class="fs-3">9</span><span class="fs-5">PM</span>
+                    <span class="fs-3">11</span><span class="fs-5">AM</span> - <span class="fs-3">6</span><span class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
                     <span class="fs-3 fw-bold text-uppercase">Sun</span>
