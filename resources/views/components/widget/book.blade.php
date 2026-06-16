6ff6c6842b0852041addb4439689ed1e25b54195
Donatien Kamary <donatkamary@gmail.com>
2026-06-16 14:13:47 +0400

    Update booking

--- resources/views/components/widget/book.blade.php
+++ resources/views/components/widget/book.blade.php
@@ -1,12 +1,24 @@
 {{-- Book reservation --}}
 
 <div class="btn-container contact-button book-table my-4">
-    <a href="{{ route($link ?? 'booking_tracking') }}" class="text-uppercase">
+    {{-- <a href="{{ route($link ?? 'booking_tracking') }}" class="text-uppercase">
         Book a table
+    </a> --}}
+    <a href="https://eatapp.co/reserve/2beachclub?source=iframe" onclick="eatapp_ShowWidget.apply(this, arguments)" target="_blank">
+        BOOK TABLE
     </a>
 </div>
 
 @once
+
+    {{-- <div style="max-width: 480px; margin: 0 auto;" id="iframe-widget">
+        <iframe
+        title="Widget config"
+        frameborder="0"
+        src="https://eatapp.co/reserve/2beachclub?source=iframe"
+        style="width: 100%; height: 80dvh; border: none; display: block;"
+        ></iframe>
+    </div> --}}
 
     @push('head')
 
@@ -24,6 +36,10 @@
 
     </style>
 
+    @endpush
+
+    @push('footer')
+        <script type="text/javascript" src="https://d183cnjuwjcs99.cloudfront.net/assets/widget/widget-iframe.min.js" async></script>
     @endpush
 
 @endonce
