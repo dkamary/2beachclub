3fd4b25fbe9f9e830c61f06d1b0a9114cefff7da
Donatien Kamary <donatkamary@gmail.com>
2026-07-15 09:53:54 +0400

    feat: implement v2 home page layout and opening hours component

--- resources/views/components/v2/opening-hours.blade.php
+++ resources/views/components/v2/opening-hours.blade.php
@@ -6,17 +6,9 @@
 
 <div @class($class)>
 
-    <x-widget.section
-        :id="$id ?? null"
-        :lazyload="true"
-        :bg-image="asset('v2/img/opening/Opening Hours.webp')"
-        :bg-image-mini="asset('v2/img/opening/Opening Hours-1.webp')"
-        :bg-class="['w-100']"
-
-        :text-class="['bg-white']"
-        text-placement="center"
-        text-size="col-11 col-md-10 mx-auto"
-    >
+    <x-widget.section :id="$id ?? null" :lazyload="true" :bg-image="asset('v2/img/opening/Opening Hours.webp')"
+        :bg-image-mini="asset('v2/img/opening/Opening Hours-1.webp')" :bg-class="['w-100']" :text-class="['bg-white']"
+        text-placement="center" text-size="col-11 col-md-10 mx-auto">
 
         <div class="row mb-3">
             <div class="col-12">
@@ -28,7 +20,8 @@
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">10</span><span class="fs-5">PM</span>
+                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">11</span><span
+                        class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
                     <span class="fs-3 fw-bold text-uppercase">Mon</span>
@@ -37,25 +30,30 @@
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">10</span><span class="fs-5">PM</span>
+                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">11</span><span
+                        class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
-                    <span class="fs-3 fw-bold text-uppercase">Tues</span> - <span class="fs-3 fw-bold text-uppercase">Thurs</span>
+                    <span class="fs-3 fw-bold text-uppercase">Tues</span> - <span
+                        class="fs-3 fw-bold text-uppercase">Thurs</span>
                 </div>
             </div>
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">11</span><span class="fs-5">PM</span>
+                    <span class="fs-3">10:30</span><span class="fs-5">AM</span> - <span class="fs-3">11</span><span
+                        class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
-                    <span class="fs-3 fw-bold text-uppercase">Fri</span> - <span class="fs-3 fw-bold text-uppercase">Sat</span>
+                    <span class="fs-3 fw-bold text-uppercase">Fri</span> - <span
+                        class="fs-3 fw-bold text-uppercase">Sat</span>
                 </div>
             </div>
 
             <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center flex-column mb-3">
                 <div class="hours text-center mb-2">
-                    <span class="fs-3">11</span><span class="fs-5">AM</span> - <span class="fs-3">6</span><span class="fs-5">PM</span>
+                    <span class="fs-3">11</span><span class="fs-5">AM</span> - <span class="fs-3">6</span><span
+                        class="fs-5">PM</span>
                 </div>
                 <div class="days text-center mb-2">
                     <span class="fs-3 fw-bold text-uppercase">Sun</span>
